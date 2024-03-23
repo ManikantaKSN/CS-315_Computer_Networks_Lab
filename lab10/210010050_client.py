5f15c4dc7e6d0733300daa06d9de6855d58e9e74
ManikantaKSN <manikantaksn28@gmail.com>
2024-03-23 10:20:04 +0530

    Adding lab10

--- lab10/210010050_client.py
+++ lab10/210010050_client.py
@@ -3,41 +3,43 @@ from socket import *
 
 serverName = 'localhost'
 serverPort = 12000
-num_pings = 10
 
+#creating UDP socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(1)
 
 total_time = 0
-received_responses = 0
+no_of_responses = 0
 print('\n----------------------------------------')
 
-for i in range(1, num_pings + 1):
+# for 10 pings
+for i in range(1, 11):
     
     start_time = time.time()
-    message = f'Ping {i} {start_time}'
-    clientSocket.sendto(message.encode(), (serverName, serverPort))
+    msg = f'Ping {i} {time.ctime(start_time)}'
+    clientSocket.sendto(msg.encode(), (serverName, serverPort))
 
     try:
-        modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
-        end_time = time.time()
-        rtt = end_time - start_time
-        print(f'PING {i}\nResponse from server: {modifiedMessage.decode()}')
+        modified_msg, server_addr = clientSocket.recvfrom(1024)
+        rtt = time.time() - start_time
+        print(f'PING SEQ NO: {i}\nResponse from server: {modified_msg.decode()}')
         print(f'RTT: {rtt} seconds')
         print('----------------------------------------')
         total_time += rtt
-        received_responses += 1
+        no_of_responses += 1
 
     except timeout:
-        print(f'PING {i}\nRequest timed out')
+        print(f'PING SEQ NO: {i}\nRequest timed out')
         print('----------------------------------------')
 
-packet_loss_rate = (num_pings - received_responses) / num_pings * 100
-print('\nPing Statistics')
+packet_loss_rate = (10 - no_of_responses) / 10 * 100
+print()
+print('Ping Statistics')
 print('----------------')
 print(f'Packet loss rate: {packet_loss_rate}%')
-if received_responses > 0:
-    avg_rtt = total_time / received_responses
+
+if no_of_responses > 0:
+    avg_rtt = total_time / no_of_responses
     print(f'Average RTT: {avg_rtt} seconds')
     print()
 clientSocket.close()
